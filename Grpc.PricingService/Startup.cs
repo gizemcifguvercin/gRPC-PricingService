a6d7640c3e6243a41e3db3b27508814f40fa885b
gizemcifguvercin <gizemcifguvercin@gmail.com>
2021-04-04 17:53:37 +0300

    Update Startup.cs

--- Grpc.PricingService/Startup.cs
+++ Grpc.PricingService/Startup.cs
@@ -9,44 +9,18 @@ namespace Grpc.PricingService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Grpc.PricingService", Version = "v1" });
-            });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grpc.PricingService v1"));
-            }
-
-            app.UseHttpsRedirection();
-
+        { 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<PricingService>();
-                endpoints.MapControllers();
             });
         }
     }
